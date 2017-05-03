18462fa3c87d70830255148a2b6e868b4f27ed4b
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-05-03 08:23:12 +0200

    LCD display and its mock implementation. Names refactor.

--- src/test/java/jaskiewicz/dev/pointOfSale/data/ProductsDatabaseTest.java
+++ src/test/java/jaskiewicz/dev/pointOfSale/data/ProductsDatabaseTest.java
@@ -1,7 +1,7 @@
 package jaskiewicz.dev.pointOfSale.data;
 
 import jaskiewicz.dev.pointOfSale.data.product.ProductNotFoundException;
-import jaskiewicz.dev.pointOfSale.data.product.ProductProvider;
+import jaskiewicz.dev.pointOfSale.data.product.ProductsDatabase;
 import jaskiewicz.dev.pointOfSale.dependencies.DependencyProvider;
 import jaskiewicz.dev.pointOfSale.model.Barcode;
 import jaskiewicz.dev.pointOfSale.model.Money;
@@ -15,23 +15,23 @@ import static org.junit.Assert.fail;
 /**
  * Created by michaljaskiewicz on 02-May-17.
  */
-public class ProductProviderTest {
+public class ProductsDatabaseTest {
 
-    private ProductProvider database;
+    private ProductsDatabase database;
 
     @Before
     public void setup() {
-        database = DependencyProvider.provideProductProvider();
+        database = DependencyProvider.provideProductsDatabase();
     }
 
     @Test
-    public void should_provide_product_with_given_barcode_when_it_is_in_database() {
+    public void should_find_product_with_given_barcode_when_it_is_in_database() {
         // given
         final Barcode barcode = new Barcode("a1a1a1a1a1");
         final String name = "Potato chips";
         final Money price = new Money(2.49);
         // when
-        final Product product = database.provideProductWith(barcode);
+        final Product product = database.findProductWith(barcode);
         // then
         assertEquals(barcode, product.getBarcode());
         assertEquals(name, product.getName());
@@ -39,13 +39,13 @@ public class ProductProviderTest {
     }
 
     @Test
-    public void should_throw_exception_when_there_is_no_product_with_given_barcode_in_database() {
+    public void should_not_find_product_when_there_is_no_product_with_given_barcode_in_database() {
         // given
         final Barcode barcode = new Barcode("qwertyuiop");
         // when
         try {
-            database.provideProductWith(barcode);
-            fail("Should throw an exception when there is no product with given barcode in database!");
+            database.findProductWith(barcode);
+            fail("Should not find product when there is no product with given barcode in database!");
         } catch (ProductNotFoundException e) {
             // then pass
         }
