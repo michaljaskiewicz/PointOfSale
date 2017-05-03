87141a3d9b2b4968fb250b60f77112fc3b1cd8f9
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-05-03 18:13:52 +0200

    Bill test refactoring.

--- src/test/java/jaskiewicz/dev/pointofsale/model/BillTest.java
+++ src/test/java/jaskiewicz/dev/pointofsale/model/BillTest.java
@@ -1,10 +1,15 @@
 package jaskiewicz.dev.pointofsale.model;
 
+import jaskiewicz.dev.pointofsale.data.product.mock.MockProductsDatabase;
+import jaskiewicz.dev.pointofsale.dependencies.DependencyProvider;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by michaljaskiewicz on 03-May-17.
@@ -40,24 +45,29 @@ public class BillTest {
     public void should_be_possible_to_add_many_products_and_calculate_total_sum() {
         // given
         final Bill bill = new Bill();
-        final Product p1 = new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49));
-        final Product p2 = new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00));
-        final Product p3 = new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49));
-        final Product p4 = new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00));
-        final Product p5 = new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49));
-        final Product p6 = new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00));
-        final Product p7 = new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49));
-        final Product p8 = new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00));
+        final List<Product> products = new LinkedList<>();
+        products.add(new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49)));
+        products.add(new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00)));
+        products.add(new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49)));
+        products.add(new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00)));
+        products.add(new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49)));
+        products.add(new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00)));
+        products.add(new Product(new Barcode("c3c3c3c3c3"), "Frozen pizza", new Money(10.49)));
+        products.add(new Product(new Barcode("4444444444"), "Eggs 6-pack", new Money(10.00)));
         // when
-        bill.add(p1);
-        bill.add(p2);
-        bill.add(p3);
-        bill.add(p4);
-        bill.add(p5);
-        bill.add(p6);
-        bill.add(p7);
-        bill.add(p8);
+        products.forEach(bill::add);
         // then
-        assertEquals(new Money(81.96).getValue(), bill.getTotalSum().getValue());
+        assertEquals(new Money(81.96), bill.getTotalSum());
+    }
+
+    @Test
+    public void should_bill_contain_all_added_products() {
+        // given
+        final List<Product> products = new MockProductsDatabase().getProducts();
+        final Bill bill = new Bill();
+        // when
+        products.forEach(bill::add);
+        // then
+        assertTrue(bill.getProducts().containsAll(products));
     }
 }
